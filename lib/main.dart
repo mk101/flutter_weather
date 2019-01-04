0aef79b595cc8a2fc28275812e0c1300a957f7bd
mk101 <kolesov.mk@yandex.ru>
2019-01-04 14:15:23 +0300

    add appbar

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,17 @@ class MyApp extends StatelessWidget {
       title: 'Weather',
       theme: ThemeData(
         primarySwatch: MaterialColor(0xFF7D64FF, <int, Color>{
-          500: Color(0xFF7D64FF)
+          50: Color(0xFF7D64FF),
+          100: Color(0xFF7D64FF),
+          200: Color(0xFF7D64FF),
+          300: Color(0xFF7D64FF),
+          400: Color(0xFF7D64FF),
+          500: Color(0xFF7D64FF),
+          600: Color(0xFF7D64FF),
+          700: Color(0xFF7D64FF),
+          800: Color(0xFF7D64FF),
+          900: Color(0xFF7D64FF),
+
         }),
       ),
       home: HomePage(title: 'Weather'),
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,6 +12,92 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: true,
+            centerTitle: true,
+            expandedHeight: 350.0,
+            actions: getActions(),
+            leading: IconButton(
+              icon: Icon(Icons.near_me),
+              onPressed: () {},
+              color: Colors.white,
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Moscow'),
+              centerTitle: true,
+              collapseMode: CollapseMode.pin,
+              background: Container(
+                color: Theme.of(context).primaryColor,
+                child: SafeArea(
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 18.0),
+                        child: Text('Now',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white
+                          ),
+                        ),
+                      ),
+                      Text('FRI, 04.01.19',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w100,
+                          fontSize: 12.0
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.only(top: 80.0),),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text('-7°', style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 54.0
+                            ),
+                          ),
+                          Text('C', style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 34.0
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text('Cloudy', style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w100,
+                          fontSize: 14.0
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              <Widget>[
+
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  
+  getActions() {
+    return <Widget> [
+      IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.settings),
+        color: Colors.white,
+      )
+    ];
   }
 }
