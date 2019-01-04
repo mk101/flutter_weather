84674973aa4a93f61c72b55a5fd0e4c15fdade39
mk101 <kolesov.mk@yandex.ru>
2019-01-04 15:00:27 +0300

    add next week section

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -82,9 +83,19 @@ class _HomePageState extends State<HomePage> {
           SliverList(
             delegate: SliverChildListDelegate(
               <Widget>[
-
+                Padding(
+                  padding: const EdgeInsets.only(top: 9.0, left: 10.0),
+                  child: Text('Next week', style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500
+                    ),
+                  ),
+                ),
               ],
             ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(getWeekItems()),
           ),
         ],
       ),
@@ -99,5 +110,92 @@ class _HomePageState extends State<HomePage> {
         color: Colors.white,
       )
     ];
+  }
+
+  getWeekItems() {
+    var widgets = <Widget>[];
+
+    var time = DateTime.now();
+    var format = DateFormat("dd.MM.yy");
+
+    for (int i = 0; i < 7; i++) {
+      String d;
+
+      if (i == 0) {
+        d = 'Today,';
+      } else if (i == 1) {
+        d = 'Tomorrow,';
+      } else {
+        switch (time.weekday) {
+          case 1:
+            d = 'Monday,';
+            break;
+          case 2:
+            d = 'Tuesday,';
+            break;
+          case 3:
+            d = 'Wednesday,';
+            break;
+          case 4:
+            d = 'Thursday,';
+            break;
+          case 5:
+            d = 'Friday,';
+            break;
+          case 6:
+            d = 'Saturday,';
+            break;
+          case 7:
+            d = 'Sunday,';
+            break;
+        }
+      }
+
+      var timeString = format.format(time);
+
+      time = time.add(Duration(days: 1));
+
+      widgets.add(ListTile(
+        title: Text(d, style: TextStyle(
+            color: Theme.of(context).accentColor,
+            fontSize: 20
+        ),
+        ),
+        subtitle: Text(timeString, style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w100,
+            color: Colors.black
+          ),
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text('-8°', style: TextStyle(
+                fontSize: 20.0
+              ),
+            ),
+            Text('C', style: TextStyle(
+                fontSize: 16.0
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(left: 5),),
+            Text('-9°', style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.black54,
+                fontWeight: FontWeight.w300
+              ),
+            ),
+            Text('C', style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.black54,
+                fontWeight: FontWeight.w300
+              ),
+            ),
+          ],
+        ),
+      ));
+    }
+
+    return widgets;
   }
 }
